6e7e346e0084ce512587a7f74b128de7284b24ef
Madprofe55or <willtparker@gmail.com>
2017-10-24 16:11:49 -0400

    updates

--- 7-Wonders-Duel-Game-Engine/CardDestroyerState.h
+++ 7-Wonders-Duel-Game-Engine/CardDestroyerState.h
@@ -9,7 +9,25 @@ public:
 	// Need pointer to the game in order to access state changes, render window, and manager functions
 	Game * p_game;
 	GamePlayingState * p_gameplayingstate;
+	Player * p_player;
 
+	// Sprites
+	sf::Sprite mMouseoverCard;
+	vector<sf::Sprite*> mCardSprites;
+
+	// Shapes
+	sf::RectangleShape mainArea;
+	vector<sf::FloatRect*> mRectCards;
+
+	// Text
+	sf::Text mMainText;
+
+	// Integers to hold card indices
+	vector<int*> mCardIndices;
+
+	const sf::Vector2f MAIN_AREA_SIZE = { 800.0f, 450.0f };
+
+	bool mouseover = false;
 
 	virtual void draw(const float dt);
 	virtual void update(const float dt);
